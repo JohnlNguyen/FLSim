33c84c6f79f5208a90e65d580ce7bf1a7bab9ea1
John Nguyen <ngjhn@fb.com>
2022-04-11 17:09:49 +0000

    clean up sync trainer

--- flsim/trainers/sync_trainer.py
+++ flsim/trainers/sync_trainer.py
@@ -58,7 +58,8 @@ class SyncTrainer(FLTrainer):
     ):
         init_self_cfg(
             self,
-            component_class=__class__,  # pyre-fixme[10]: Name `__class__` is used but not defined.
+            # pyre-fixme[10]: Name `__class__` is used but not defined.
+            component_class=__class__,
             config_class=SyncTrainerConfig,
             **kwargs,
         )
@@ -165,7 +166,6 @@ class SyncTrainer(FLTrainer):
         FLDistributedUtils.setup_distributed_training(
             distributed_world_size, use_cuda=self.cuda_enabled
         )  # TODO do not call distributed utils here, this is upstream responsibility
-        self.logger.info(f" dist world size = {distributed_world_size}")
 
         if rank != 0:
             FLDistributedUtils.suppress_output()
@@ -179,26 +179,15 @@ class SyncTrainer(FLTrainer):
         users_per_round = min(self.cfg.users_per_round, num_total_users)
 
         self.data_provider = data_provider
-        num_rounds_in_epoch = self.rounds_in_one_epoch(num_total_users, users_per_round)
+        num_rounds_in_epoch = self.rounds_in_one_epoch(
+            num_total_users, users_per_round)
         num_users_on_worker = data_provider.num_train_users()
-        self.logger.debug(
-            f"num_users_on_worker: {num_users_on_worker}, "
-            f"users_per_round: {users_per_round}, "
-            f"num_total_users: {num_total_users}"
-        )
-        # torch.multinomial requires int instead of float, cast it as int
-        users_per_round_on_worker = int(users_per_round / distributed_world_size)
-        self._validate_users_per_round(users_per_round_on_worker, num_users_on_worker)
 
-        self.logger.info("Start training")
-        if self.logger.isEnabledFor(logging.DEBUG):
-            norm = FLModelParamUtils.debug_model_norm(
-                self.global_model().fl_get_module()
-            )
-            self.logger.debug(
-                self.cuda_enabled and distributed_world_size > 1,
-                f"from worker {rank}: model norm is {norm} after round {iter}",
-            )
+        # torch.multinomial requires int instead of float, cast it as int
+        users_per_round_on_worker = int(
+            users_per_round / distributed_world_size)
+        self._validate_users_per_round(
+            users_per_round_on_worker, num_users_on_worker)
 
         # main training loop
         num_int_epochs = math.ceil(self.cfg.epochs)
@@ -215,7 +204,6 @@ class SyncTrainer(FLTrainer):
                     epoch=epoch, round=round, rounds_per_epoch=num_rounds_in_epoch
                 )
 
-                t = time()
                 clients = self._client_selection(
                     num_users_on_worker,
                     users_per_round_on_worker,
@@ -223,50 +211,18 @@ class SyncTrainer(FLTrainer):
                     self.global_model(),
                     epoch,
                 )
-                self.logger.info(f"Client Selection took: {time() - t} s.")
 
-                agg_metric_clients = self._choose_clients_for_post_aggregation_metrics(
-                    train_clients=clients,
-                    num_total_users=num_users_on_worker,
-                    users_per_round=users_per_round_on_worker,
-                )
-
-                # training on selected clients for the round
-                self.logger.info(f"# clients/round on worker {rank}: {len(clients)}.")
                 self._train_one_round(
                     timeline=timeline,
                     clients=clients,
-                    agg_metric_clients=agg_metric_clients,
                     users_per_round=users_per_round,
                     metric_reporter=metric_reporter
                     if self.cfg.report_train_metrics
                     else None,
                 )
 
-                if self.logger.isEnabledFor(logging.DEBUG):
-                    norm = FLModelParamUtils.debug_model_norm(
-                        self.global_model().fl_get_module()
-                    )
-                    self.logger.debug(
-                        self.cuda_enabled and distributed_world_size > 1,
-                        f"from worker {rank}: model norm: {norm} @ "
-                        f"epoch:{epoch}, round:{round}",
-                    )
-
                 # report training success rate and training time variance
                 if rank == 0:
-                    if (
-                        self._timeout_simulator.sample_mean_per_user != 0
-                        or self._timeout_simulator.sample_var_per_user != 0
-                    ):
-                        self.logger.info(
-                            f"mean training time/user: "
-                            f"{self._timeout_simulator.sample_mean_per_user}",
-                            f"variance of training time/user: "
-                            f"{self._timeout_simulator.sample_var_per_user}",
-                        )
-
-                    t = time()
                     (best_metric, best_model_state,) = self._maybe_run_evaluation(
                         timeline=timeline,
                         data_provider=data_provider,
@@ -274,18 +230,16 @@ class SyncTrainer(FLTrainer):
                         best_metric=best_metric,
                         best_model_state=best_model_state,
                     )
-                    self.logger.info(f"Evaluation took {time() - t} s.")
 
                 if self.stop_fl_training(
                     epoch=epoch, round=round, num_rounds_in_epoch=num_rounds_in_epoch
                 ):
                     break
 
-            # pyre-fixme[61]: `timeline` may not be initialized here.
-            self._post_epoch_client_metrics_eval(timeline, metric_reporter)
             if self.stop_fl_training(
                 epoch=epoch,
-                round=round,  # pyre-fixme[61]: `round` may not be initialized here.
+                # pyre-fixme[61]: `round` may not be initialized here.
+                round=round,
                 num_rounds_in_epoch=num_rounds_in_epoch,
             ):
                 break
@@ -295,23 +249,14 @@ class SyncTrainer(FLTrainer):
 
         return self.global_model(), best_metric
 
-    def _post_epoch_client_metrics_eval(
-        self,
-        timeline: Timeline,
-        metric_reporter: IFLMetricsReporter,
-    ):
-        self._report_post_epoch_client_metrics(
-            timeline=timeline,
-            metric_reporter=metric_reporter,
-        )
-
     def stop_fl_training(self, *, epoch, round, num_rounds_in_epoch) -> bool:
         # stop if necessary number of steps/epochs are completed in case of fractional epochs
         # or if client times out
         global_round_num = (epoch - 1) * num_rounds_in_epoch + round
         return (
             (global_round_num / num_rounds_in_epoch)
-            >= self.cfg.epochs  # pyre-fixme[16]: `SyncTrainer` has no attribute `cfg`.
+            # pyre-fixme[16]: `SyncTrainer` has no attribute `cfg`.
+            >= self.cfg.epochs
             or self._timeout_simulator.stop_fl()
         )
 
@@ -321,7 +266,8 @@ class SyncTrainer(FLTrainer):
         """
         sort users by their training time, and only keep num_users_keep users
         """
-        all_training_times = [c.get_total_training_time() for c in clents_triggered]
+        all_training_times = [c.get_total_training_time()
+                              for c in clents_triggered]
         all_training_times.sort()
         # only select first num_users_keep userids sort by their finish time
         num_users_keep = min([num_users_keep, len(all_training_times)])
@@ -344,11 +290,11 @@ class SyncTrainer(FLTrainer):
         num_users: int,
         users_per_round: int,
         data_provider: IFLDataProvider,
-        global_model: IFLModel,
         epoch: int,
     ) -> List[Client]:
         # pyre-fixme[16]: `SyncTrainer` has no attribute `cfg`.
-        num_users_overselected = math.ceil(users_per_round / self.cfg.dropout_rate)
+        num_users_overselected = math.ceil(
+            users_per_round / self.cfg.dropout_rate)
         user_indices_overselected = self.server.select_clients_for_training(
             num_total_users=num_users,
             users_per_round=num_users_overselected,
@@ -371,43 +317,24 @@ class SyncTrainer(FLTrainer):
         self,
         timeline: Timeline,
         clients: Iterable[Client],
-        agg_metric_clients: Iterable[Client],
         users_per_round: int,
         metric_reporter: Optional[IFLMetricsReporter],
     ) -> None:
-        """Args:
-        timeline: information about the round, epoch, round number, ...
-        clients: clients for the round
-        agg_metric_clients: clients for evaluating the post-aggregation training metrics
-        metric_reporter: the metric reporter to pass to other methods
-        """
-        t = time()
         self.server.init_round()
-        self.logger.info(f"Round initialization took {time() - t} s.")
-
-        def update(client):
+        for client in clients:
             client_delta, weight = client.generate_local_update(
                 self.global_model(), metric_reporter
             )
-            self.server.receive_update_from_client(Message(client_delta, weight))
-
-        t = time()
-        for client in clients:
-            update(client)
-        self.logger.info(f"Collecting round's clients took {time() - t} s.")
-
-        t = time()
+            message = Message(client_delta, weight)
+            self.server.receive_update_from_client(message)
         self.server.step()
-        self.logger.info(f"Finalizing round took {time() - t} s.")
 
-        t = time()
         self._report_train_metrics(
             model=self.global_model(),
             timeline=timeline,
             metric_reporter=metric_reporter,
         )
         self._evaluate_global_model_after_aggregation(
-            clients=agg_metric_clients,
             model=self.global_model(),
             timeline=timeline,
             users_per_round=users_per_round,
@@ -417,41 +344,9 @@ class SyncTrainer(FLTrainer):
             timeline,
             metric_reporter,
         )
-        self.logger.info(f"Aggregate round reporting took {time() - t} s.")
-
-    def _choose_clients_for_post_aggregation_metrics(
-        self,
-        train_clients: Iterable[Client],
-        num_total_users: int,
-        users_per_round: int,
-    ) -> Iterable[Client]:
-        """
-        Chooses clients for the post-aggregation training metrics.
-        Depending on config parameters, either return the round's
-        training clients or new randomly drawn clients.
-        """
-        # pyre-fixme[16]: `SyncTrainer` has no attribute `cfg`.
-        if self.cfg.use_train_clients_for_aggregation_metrics:
-            return train_clients
-
-        # For the post-aggregation metrics, evaluate on new users
-        agg_metric_client_idcs = torch.multinomial(
-            torch.ones(num_total_users, dtype=torch.float),
-            users_per_round,
-            replacement=False,
-        ).tolist()
-
-        agg_metric_clients = [
-            self.create_or_get_client_for_data(i, self.data_provider)
-            for i in agg_metric_client_idcs
-        ]
-        return agg_metric_clients
 
     def _calc_privacy_metrics(
-        self,
-        clients: Iterable[Client],
-        model: IFLModel,
-        metric_reporter: Optional[IFLMetricsReporter],
+        self, clients: Iterable[Client],
     ) -> List[Metric]:
         """
         Calculates privacy metrics.
@@ -508,30 +403,6 @@ class SyncTrainer(FLTrainer):
 
         return metrics
 
-    def calc_post_epoch_client_metrics(
-        self,
-        client_models: Dict[Client, IFLModel],
-        round_timeline: Timeline,
-        metric_reporter: IFLMetricsReporter,
-    ) -> List[List[Metric]]:
-        """
-        Calculates client-side metrics on the overall evaluation set
-        """
-        client_metrics = []
-        if metric_reporter is not None:
-            for client, model in tqdm(client_models.items()):
-                metric_reporter.reset()
-                client.eval(
-                    model=model,
-                    metric_reporter=metric_reporter,
-                )
-                # pyre-fixme[16]: `IFLMetricsReporter` has no attribute
-                #  `compute_scores`.
-                score = metric_reporter.compute_scores()
-                client_metrics.append(Metric.from_dict(score))
-
-        return client_metrics
-
     def _evaluate_global_model_after_aggregation(
         self,
         clients: Iterable[Client],
@@ -581,63 +452,6 @@ class SyncTrainer(FLTrainer):
             "Users per round is greater than number of users in data provider for the worker."
             "If you are using paged dataloader, increase your num_users_per_page >> users_per_round"
         )
-
-    def _report_post_epoch_client_metrics(
-        self,
-        timeline: Timeline,
-        metric_reporter: Optional[IFLMetricsReporter],
-    ):
-        if (
-            metric_reporter is not None
-            # pyre-fixme[16]: `SyncTrainer` has no attribute `cfg`.
-            and self.cfg.report_client_metrics
-            and self.cfg.report_client_metrics_after_epoch
-            and (timeline.epoch % self.cfg.client_metrics_reported_per_epoch == 0)
-        ):
-            client_models = {
-                client: client.last_updated_model for client in self.clients.values()
-            }
-            client_scores = self.calc_post_epoch_client_metrics(
-                client_models, timeline, metric_reporter
-            )
-
-            # Find stats over the client_metrics (mean, min, max, median, std)
-            client_stats_trackers = {}
-            score_names = [metric.name for metric in next(iter(client_scores))]
-            for score_name in score_names:
-                client_stats_trackers[score_name] = RandomVariableStatsTracker(
-                    tracks_quantiles=True
-                )
-            for client_metric_list in client_scores:
-                for client_metric in client_metric_list:
-                    client_stats_trackers[client_metric.name].update(
-                        client_metric.value
-                    )
-
-            reportable_client_metrics = []
-            for score_name in score_names:
-                for stat_name, stat_key in [
-                    ("Mean", "mean_val"),
-                    ("Median", "median_val"),
-                    ("Upper Quartile", "upper_quartile_val"),
-                    ("Lower Quartile", "lower_quartile_val"),
-                    ("Min", "min_val"),
-                    ("Max", "max_val"),
-                    ("Standard Deviation", "standard_deviation_val"),
-                    ("Num Samples", "num_samples"),
-                ]:
-                    score = client_stats_trackers[score_name].__getattribute__(stat_key)
-                    reportable_client_metrics.append(Metric(stat_name, score))
-
-            metric_reporter.report_metrics(
-                model=None,
-                reset=True,
-                stage=TrainingStage.PER_CLIENT_EVAL,
-                timeline=timeline,
-                epoch=timeline.global_round_num(),  # for legacy
-                print_to_channels=True,
-                extra_metrics=reportable_client_metrics,
-            )
 
     @staticmethod
     def rounds_in_one_epoch(num_total_users: int, users_per_round: int) -> int:
